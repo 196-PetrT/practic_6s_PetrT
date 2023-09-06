7cb6a2c6087a016d85bfd2d429b43c8e601bb890
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-06 22:54:22 +0300

    simple code

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -3,3 +3,17 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+Console.Write("Введите элементы(через пробел): ");
+int[] result = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+int GetSumMoreZerro()
+{
+  int count = 0;
+  for (int i = 0; i < result.Length; i++)
+  {
+    if (result[i] > 0) count++;
+  }
+  return count;
+}
+
+Console.WriteLine($"Количество чисел больше 0: {GetSumMoreZerro()}");
--- Task43/Program.cs
+++ Task43/Program.cs
@@ -3,3 +3,24 @@
 // b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+int SetNumber(string message = " ") // добавим метод для получения чисел вводом с консоли
+{
+  System.Console.Write($"Enter number {message} : ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int factorB1 = SetNumber("b1");
+int factorK1 = SetNumber("k1");
+int factorB2 = SetNumber("b2");
+int factorK2 = SetNumber("k2");
+
+Console.WriteLine($"{(factorK1 == factorK2 ? "прямые параллельны или совпадают" : "прямые пересекаются в точке :")}");
+
+if (factorK1 < factorK2 || factorK1 > factorK2) 
+{
+double x = (factorB2 - factorB1) / (factorK1 - factorK2);
+double y = factorK1 * x + factorB1;
+Console.Write($"{x}, {y}");
+}
+
+
